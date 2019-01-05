d17d777426d2e90fbda409d3b36fec6e80c21d1a
Brandon Biggs <brandonkeithbiggs@gmail.com>
2019-01-04 20:29:23 -0800

    Added new wheel and pkg files

---
+++ build/lib/accessible_output2/libloader/__init__.py
@@ -0,0 +1,7 @@
+from accessible_output2.libloader.libloader import *
+
+__version__ = 0.1
+__author__ = 'Christopher Toth <q@q-continuum.net>'
+__doc__ = """
+Quickly and easily load shared libraries from various platforms. Also includes a libloader.com module for loading com modules on Windows.
+"""
